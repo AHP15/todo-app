3b5b89bdeac4fd0cd2d4217577c4c5d436d7b51a
Abdessittir <harkati.web.dev@gmail.com>
2023-10-31 12:28:27 +0100

    Create event loop

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,25 @@
 int main()
 {
     Window window { 680, 480 };
-    window.show();
+
+    bool running { true };
+    while (running)
+    {
+        SDL_Event event { window.getEvent() };
+        while (SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        window.show();
+        Button button { "Click me", { 680/2, 480/2 } };
+        window.RenderButton(button);
+        if(!running) break;
+    }
+    
     
     return 0;
 }
